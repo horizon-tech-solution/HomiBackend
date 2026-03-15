203ba04a4aaf1917940fa170747d399a6c84f63d
MushiehEdison <mushiehedison66@gmal.com>
2026-03-15 11:47:27 +0100

    live

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-header('Access-Control-Allow-Origin: http://localhost:5173');
+header('Access-Control-Allow-Origin: https://prop-ty.netlify.app/');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Access-Control-Allow-Credentials: true');
