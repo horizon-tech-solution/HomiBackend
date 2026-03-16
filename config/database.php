015cdbc1e2a7d5bb394a96be1c955649e8cec32e
MushiehEdison <mushiehedison66@gmal.com>
2026-03-16 10:26:07 +0100

    back

--- config/database.php
+++ config/database.php
@@ -28,7 +28,6 @@ class Database {
                 PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES   => false,
-                Pdo\Mysql::ATTR_FOUND_ROWS   => true,  // fixed: was PDO::MYSQL_ATTR_FOUND_ROWS
             ]);
         } catch (PDOException $e) {
             http_response_code(500);
